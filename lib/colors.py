42fa323a038b5f492bebd6403af1b5bc524f6904
Cx99 <136205231+cexploit99@users.noreply.github.com>
2026-01-13 06:40:20 +0700

    Add files via upload

---
+++ lib/colors.py
@@ -0,0 +1,13 @@
+from colorama import Fore, Back, Style
+
+r = Fore.RED
+g = Fore.GREEN
+b = Fore.BLUE
+wh = Fore.WHITE
+yl = Fore.YELLOW
+res = Style.RESET_ALL
+
+bg_gr = Back.GREEN
+bg_red = Back.RED
+bg_wh = Back.WHITE
+bg_y = Back.YELLOW
